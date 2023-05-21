5102679b931b35882b7c71e716b9c728d0868bd1
Olga Yurina <olga.nik.yurina@gmail.com>
2023-05-21 14:55:44 +0300

    Add limits commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 fun main() {
-    val amountCommission = calculateCommission("Maestro",7_000_00, 100_000_00)
-
-    println("Комиссия за перевод " + amountCommission/100 + " руб. " + amountCommission%100 + " коп.")
-
+    println(resultPayment("Maestro", 7_000_00, 100_000_00))
 }
 
-fun calculateCommission (typeCardAccount:String, amountOfPayCurrentMonth:Int = 0, amountCurrentPay: Int): Int {
+fun calculateCommission (typeCardAccount:String, amountOfPayCurrentMonth:Int = 0, amountCurrentPay: Int): String {
     val levelFreeCommissionMasterCardMaestro = 75_000_00
     val amountPayOverLevelMasterCardMaestro = amountCurrentPay + amountOfPayCurrentMonth - levelFreeCommissionMasterCardMaestro
     val commissionOverLevelMasterCardMaestro = 0.06
@@ -14,13 +11,36 @@ fun calculateCommission (typeCardAccount:String, amountOfPayCurrentMonth:Int = 0
     val minCommissionVisaMir = 35_00
     val commission = when (typeCardAccount) {
         "MasterCard", "Maestro" -> if ((amountOfPayCurrentMonth + amountCurrentPay) > levelFreeCommissionMasterCardMaestro)
-                                      (amountPayOverLevelMasterCardMaestro*commissionOverLevelMasterCardMaestro).toInt() + commissionOverLevelMasterCardMaestroRur
-                                   else 0
-        "Visa", "Мир"          -> if(amountCurrentPay*commissionVisaMir > minCommissionVisaMir )
-                                    (amountCurrentPay*commissionVisaMir).toInt()
-                                  else minCommissionVisaMir
+            (amountPayOverLevelMasterCardMaestro * commissionOverLevelMasterCardMaestro).toInt() + commissionOverLevelMasterCardMaestroRur
+        else 0
+        "Visa", "Мир" -> if (amountCurrentPay * commissionVisaMir > minCommissionVisaMir)
+            (amountCurrentPay * commissionVisaMir).toInt()
+        else minCommissionVisaMir
         else -> 0
     }
-    return commission
+
+
+    return "Комиссия составила " + commission/100 + " руб." + commission%100 + " коп."
 
 }
+
+fun resultPayment (typeCardAccount: String, amountOfPayCurrentMonth: Int = 0, amountCurrentPay: Int): String  {
+    val maxPaymentCardOnTime = 150_000_00
+    val maxPaymentCardMonth = 600_000_00
+    val maxPaymentVkPayOnTime = 15_000_00
+    val maxPaymentVkPayMonth = 40_000_00
+    val amountCurrentPayAll = amountCurrentPay + amountOfPayCurrentMonth
+    val result = when (typeCardAccount) {
+    "VK Pay" -> if(amountCurrentPay < maxPaymentVkPayOnTime && amountCurrentPayAll < maxPaymentVkPayMonth)
+                calculateCommission(typeCardAccount,amountOfPayCurrentMonth,amountCurrentPay)
+                else   "Лимит VK Pay превышен"
+    else -> if(amountCurrentPay < maxPaymentCardOnTime && amountCurrentPayAll < maxPaymentCardMonth)
+                calculateCommission(typeCardAccount,amountOfPayCurrentMonth,amountCurrentPay)
+                else  "Лимит карты " + typeCardAccount +  " превышен"
+
+}
+    return result
+}
+
+
+
